b7fbb9c53be1335512c2272214a0dca94ad5ee73
akaiosorani <akaiosorani@tokyo.so-net.jp>
2011-12-12 11:35:45 +0900

    * x86環境用ビルドスクリプト追加

---
+++ zadb/src/build-x86.sh
@@ -0,0 +1,15 @@
+
+CC=gcc
+ZADBDEF="-DADB_HOST=1 -DADB_TRACE_PACKETS=1"
+
+CFLAGS="-O2 -g -Wall -Wno-unused-parameter"
+LIBUSBCFLAGS="`pkg-config --cflags  --libs libusb-1.0`"
+#LIBUSBCFLAGS="-I. -L."
+SOURCES="usb_libusb.c usb_vendors.c transport_usb.c send.c transport_z.c zadb.c"
+LIBS="-lpthread "
+OUTFILE="zadb-x86"
+
+rm $OUTFILE
+
+$CC $CFLAGS $ZADBDEF $LIBUSBCFLAGS $SOURCES $LIBS -o $OUTFILE
+
