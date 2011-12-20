25b424bd5e0ca9a05abcc7828275616f5423dc2f
akaiosorani <akaiosorani@tokyo.so-net.jp>
2011-12-20 09:54:21 +0900

    * ビルドスクリプト修正

--- zadb/src/build-x86.sh
+++ zadb/src/build-x86.sh
@@ -1,11 +1,11 @@
 
 CC=gcc
-ZADBDEF="-DADB_HOST=1 -DADB_TRACE_PACKETS=1"
+ZADBDEF="-DADB_HOST=1 -DADB_TRACE_PACKETS=1 -DHAVE_TERMIO_H=1"
 
 CFLAGS="-O2 -g -Wall -Wno-unused-parameter"
 LIBUSBCFLAGS="`pkg-config --cflags  --libs libusb-1.0`"
 #LIBUSBCFLAGS="-I. -L."
-SOURCES="usb_libusb.c usb_vendors.c transport_usb.c send.c transport_z.c zadb.c"
+SOURCES="usb_libusb.c usb_vendors.c transport_usb.c send.c transport_z.c zadb.c commandline.c keycode.c"
 LIBS="-lpthread "
 OUTFILE="zadb-x86"
 
--- zadb/src/build.sh
+++ zadb/src/build.sh
@@ -1,11 +1,11 @@
 
 CC=gcc
-ZADBDEF="-DADB_HOST=1 -DADB_TRACE_PACKETS=1"
+ZADBDEF="-DADB_HOST=1 -DADB_TRACE_PACKETS=1 -DHAVE_TERMIO_H=1"
 
 CFLAGS="-O2 -g -Wall -Wno-unused-parameter"
 #LIBUSBCFLAGS="`pkg-config --cflags libusb-1.0`"
-LIBUSBCFLAGS="-I. -L."
-SOURCES="usb_libusb.c usb_vendors.c transport_usb.c send.c transport_z.c zadb.c"
+LIBUSBCFLAGS="-Ilibusb -Llibusb"
+SOURCES="usb_libusb.c usb_vendors.c transport_usb.c send.c transport_z.c zadb.c keycode.c commandline.c"
 LIBS="-lpthread -lusb-1.0"
 OUTFILE="zadb"
 
